42d981955bd1a8f00041d400722e01ee69e77e98
Ricardo Gobbo de Souza <ricardo@datalogix.com.br>
2019-08-08 09:36:41 -0300

    test: custom fixture

---
+++ test/custom-fixture.test.js
@@ -0,0 +1,18 @@
+const { setup, loadConfig, get } = require('..')
+
+describe('setup', () => {
+  let nuxt
+
+  beforeAll(async () => {
+    ({ nuxt } = await setup(loadConfig(__dirname, 'custom')))
+  }, 60000)
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  test('render', async () => {
+    const html = await get('/')
+    expect(html).toContain('Works!')
+  })
+})
