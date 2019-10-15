57f15fb87cc27895d31543cc2c9f1c22beedf853
Rafał Chłodnicki <rchl2k@gmail.com>
2019-10-15 09:41:08 +0200

    fix: don't allow mutating original config object by the caller
    `loadConfig` with `merge=false` was using spread operator to
    clone  properties from fixture's configuration. Since that is
    a shallow copy, child objects were returned by reference and
    allowed caller to mutate those objects, affecting further
    uses of `loadConfig`. Ensure that is not possible by returning
    a copy of loaded config.
    
    This was a problem when one test loaded config and passed it
    to Nuxt Builder. Nuxt builder then appended generated plugins
    to plugins object and those plugins were then used by subsequent
    tests, even if it intended to build new, clean fixture.

--- test/setup.test.js
+++ test/setup.test.js
@@ -37,4 +37,20 @@ describe('loadConfig', () => {
     const config = loadConfig(__dirname, 'setup', override, { merge: true })
     expect(config.env).toEqual({ ENV1: 'true', ENV2: 'true' })
   })
+
+  test('returns a copy of loaded config when not merging', () => {
+    let config = loadConfig(__dirname, 'setup')
+    config.render.injectScripts = true
+
+    config = loadConfig(__dirname, 'setup')
+    expect(config.render.injectScripts).toEqual(undefined)
+  })
+
+  test('returns a copy of loaded config when merging', () => {
+    const override = { render: { injectScripts: true } }
+    let config = loadConfig(__dirname, 'setup', override, { merge: true })
+
+    config = loadConfig(__dirname, 'setup')
+    expect(config.render.injectScripts).toEqual(undefined)
+  })
 })
