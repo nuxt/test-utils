531a62fe7f88da3a9a614f22b221f233510454b1
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-08-05 11:32:10 -0300

    chore: rename jest matchers

--- src/jest.matchers.ts
+++ src/jest.matchers.ts
@@ -3,23 +3,23 @@ import { NuxtTestContext } from './types'
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toNuxtPluginAdded(plugin: any): CustomMatcherResult
-      toNuxtLayoutAdded(layout: any, name?: string): CustomMatcherResult
-      toNuxtErrorLayoutAdded(errorLayout: any): CustomMatcherResult
-      toNuxtServerMiddlewareAdded(middleware: any): CustomMatcherResult
-      toNuxtRequireModule(moduleOpts: any): CustomMatcherResult
+      toHaveCalledNuxtAddPlugin(plugin: any): CustomMatcherResult
+      toHaveCalledNuxtAddLayout(layout: any, name?: string): CustomMatcherResult
+      toHaveCalledNuxtAddErrorLayout(errorLayout: any): CustomMatcherResult
+      toHaveCalledNuxtAddServerMiddleware(middleware: any): CustomMatcherResult
+      toHaveCalledNuxtRequireModule(moduleOpts: any): CustomMatcherResult
     }
   }
 }
 
 expect.extend({
-  toNuxtPluginAdded (ctx: NuxtTestContext, plugin: any) {
+  toHaveCalledNuxtAddPlugin (ctx: NuxtTestContext, plugin: any) {
     expect(ctx.nuxt.moduleContainer.addPlugin).toBeCalledWith(plugin)
 
     return { pass: true, message: () => '' }
   },
 
-  toNuxtLayoutAdded (ctx: NuxtTestContext, layout: any, name?: string) {
+  toHaveCalledNuxtAddLayout (ctx: NuxtTestContext, layout: any, name?: string) {
     if (name) {
       expect(ctx.nuxt.moduleContainer.addLayout).toBeCalledWith(layout, name)
     } else {
@@ -29,19 +29,19 @@ expect.extend({
     return { pass: true, message: () => '' }
   },
 
-  toNuxtErrorLayoutAdded (ctx: NuxtTestContext, errorLayout: any) {
+  toHaveCalledNuxtAddErrorLayout (ctx: NuxtTestContext, errorLayout: any) {
     expect(ctx.nuxt.moduleContainer.addErrorLayout).toBeCalledWith(errorLayout)
 
     return { pass: true, message: () => '' }
   },
 
-  toNuxtServerMiddlewareAdded (ctx: NuxtTestContext, middleware: any) {
+  toHaveCalledNuxtAddServerMiddleware (ctx: NuxtTestContext, middleware: any) {
     expect(ctx.nuxt.moduleContainer.addServerMiddleware).toBeCalledWith(middleware)
 
     return { pass: true, message: () => '' }
   },
 
-  toNuxtRequireModule (ctx: NuxtTestContext, moduleOpts: any) {
+  toHaveCalledNuxtRequireModule (ctx: NuxtTestContext, moduleOpts: any) {
     expect(ctx.nuxt.moduleContainer.requireModule).toBeCalledWith(moduleOpts)
 
     return { pass: true, message: () => '' }
