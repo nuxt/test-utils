d1c1c7192c8ba2ffd3609f42ac9f431d6ded474f
pooya parsa <pyapar@gmail.com>
2020-08-09 12:57:48 +0200

    chore: revert unnecessary filter

--- src/assertions.ts
+++ src/assertions.ts
@@ -3,13 +3,5 @@ import { getNuxt } from './nuxt'
 type ModuleContainerMethod = 'addPlugin' | 'addLayout' | 'addErrorLayout' | 'addServerMiddleware' | 'requireModule'
 
 export function expectModuleToBeCalledWith (method: ModuleContainerMethod, ...args) {
-  const _args = []
-  for (const arg of args) {
-    if (arg === undefined) {
-      break
-    }
-    _args.push(arg)
-  }
-
-  expect(getNuxt().moduleContainer[method]).toBeCalledWith(..._args)
+  expect(getNuxt().moduleContainer[method]).toBeCalledWith(...args)
 }
