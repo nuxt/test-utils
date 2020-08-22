229e5617c23806ef461a1a22db28daf1e4a5e8e6
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-08-22 09:14:01 -0300

    fix: create jest-preset in top level of package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import preset from './preset'
-
 export * from './assertions'
 export * from './browser'
 export * from './build'
@@ -8,5 +6,3 @@ export * from './generate'
 export * from './nuxt'
 export * from './server'
 export * from './setup'
-
-export default preset
