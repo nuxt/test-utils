5bf4305d2807257bb85ffeae36c23b0089ee36ee
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-09-03 19:26:34 -0300

    feat: create jest preset (#60)
    * feat: create preset
    
    * fix: create jest-preset in top level of package
    
    * feat: add transform in preset
    
    * chore: update deps

---
+++ jest-preset.js
@@ -0,0 +1,13 @@
+module.exports = {
+  collectCoverage: true,
+  testEnvironment: 'node',
+  transform: {
+    '\\.(js|ts)$': [
+      'babel-jest',
+      {
+        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+        plugins: ['@babel/plugin-transform-runtime']
+      }
+    ]
+  }
+}
