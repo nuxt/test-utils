2f6efc8ebfd9432cb7dc68e407deac89c224b227
Pooya Parsa <pyapar@gmail.com>
2020-12-15 18:43:23 +0100

    fix(jest): use node: current as target

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  preset: './jest-preset.js',
-  collectCoverageFrom: ['src/**/*.ts']
+  collectCoverage: true,
+  testEnvironment: 'node',
+  transform: {
+    '\\.(js|ts)$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { "targets": { "node": "current" }}],
+          '@babel/preset-typescript'
+        ],
+        plugins: ['@babel/plugin-transform-runtime']
+      }
+    ]
+  }
 }
