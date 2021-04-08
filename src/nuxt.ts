f604dff709bc523c93f1f17db7a972d5a5252c03
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-04-08 09:58:59 -0300

    feat: resolve rootDir (#107)

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,3 +1,4 @@
+import { existsSync } from 'fs'
 import { resolve } from 'path'
 import { getContext } from './context'
 
@@ -8,10 +9,36 @@ export async function loadNuxt () {
   ctx.nuxt = new Nuxt(ctx.options.config)
 }
 
+const isNuxtApp = (dir: string) => {
+  return existsSync(dir) && (
+    existsSync(resolve(dir, 'pages')) ||
+    existsSync(resolve(dir, 'nuxt.config.js')) ||
+    existsSync(resolve(dir, 'nuxt.config.ts'))
+  )
+}
+
+const resolveRootDir = () => {
+  const { options } = getContext()
+
+  const dirs = [
+    options.rootDir,
+    resolve(options.testDir, options.fixture),
+    process.cwd()
+  ]
+
+  for (const dir of dirs) {
+    if (dir && isNuxtApp(dir)) {
+      return dir
+    }
+  }
+
+  throw new Error('Invalid nuxt app. (Please explicitly set `options.rootDir` pointing to a valid nuxt app)')
+}
+
 export async function loadFixture () {
   const { options } = getContext()
 
-  options.rootDir = resolve(options.testDir, options.fixture)
+  options.rootDir = resolveRootDir()
 
   const { loadNuxtConfig } = await loadNuxtPackage()
   options.config = await loadNuxtConfig({
