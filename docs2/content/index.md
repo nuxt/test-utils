16770f37b580b779a50fbb6e69df1edebb25af90
Tom Mrazauskas <tom@mrazauskas.de>
2021-07-05 08:57:13 +0300

    docs: new docs draft

---
+++ docs2/content/index.md
@@ -0,0 +1,19 @@
+---
+title: Introduction
+description: Easily set up tests for your Nuxt projects
+position: 1
+---
+
+<img src="/preview.svg" class="light-img"/>
+<img src="/preview-dark.svg" class="dark-img"/>
+
+Nuxt Test Utils aims to make it easier to set up tests for your Nuxt projects.
+
+## Features
+
+::list
+- Easy to set up tests
+- Test in browser
+- Extend Jest matchers
+- Written in TypeScript
+::
---
+++ docs2/docus.config.js
@@ -0,0 +1,22 @@
+export default {
+  title: 'Nuxt Test Utils',
+  url: 'https://test-utils.nuxtjs.org',
+  theme: {
+    colors: {
+      primary: '#00BB6F'
+    },
+    header: {
+      title: false,
+      logo: {
+        light: '/logo-light.svg',
+        dark: '/logo-dark.svg'
+      }
+    }
+  },
+  twitter: 'nuxt_js',
+  github: {
+    repo: 'nuxt/test-utils',
+    dir: 'docs',
+    branch: 'main'
+  }
+}
