670ab40598b595346fce625a1c43f21b1a722dc6
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-07-12 12:27:06 -0300

    chore: use sucrese

--- jest-preset.js
+++ jest-preset.js
@@ -1,16 +1,5 @@
 module.exports = {
   collectCoverage: true,
   testEnvironment: 'node',
-  transform: {
-    '\\.(js|ts)$': [
-      'babel-jest',
-      {
-        presets: [
-          ['@babel/preset-env', { targets: { node: 'current' } }],
-          '@babel/preset-typescript'
-        ],
-        plugins: ['@babel/plugin-transform-runtime']
-      }
-    ]
-  }
+  transform: { '\\.(js|ts)$': './transform' }
 }
---
+++ transform.js
@@ -0,0 +1,5 @@
+const { process } = require('@sucrase/jest-plugin')
+
+module.exports = {
+  process
+}
