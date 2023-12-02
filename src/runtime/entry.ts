129d83356fc92d483ab0d92c2f92add357558f8c
Daniel Roe <daniel@roe.dev>
2023-12-02 22:04:57 +0000

    fix: import `useRouter` explicitly in entry

--- src/runtime/entry.ts
+++ src/runtime/entry.ts
@@ -3,6 +3,7 @@ if (
   // @ts-expect-error undefined property
   window.__NUXT_VITEST_ENVIRONMENT__
 ) {
+  const { useRouter } = await import('#app/composables/router')
   // @ts-expect-error alias to allow us to transform the entrypoint
   await import('#app/nuxt-vitest-app-entry').then(r => r.default())
   // We must manually call `page:finish` to snc route after navigation
