d263b1bd7a82032451636158ae2be8a7a4c1d8af
Daniel Roe <daniel@roe.dev>
2023-12-05 22:10:10 +0000

    test: add `@nuxtjs/i18n` fixture

---
+++ examples/i18n/test/index.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest'
+
+import { mountSuspended } from '@nuxt/test-utils/runtime-utils'
+import App from '~/app.vue'
+
+describe('Mounting app with `@nuxtjs/i18n`', () => {
+  it('can mount some component', async () => {
+    const component = await mountSuspended(App)
+    expect(component.vm).toBeTruthy()
+    expect(component.text()).toMatchInlineSnapshot(
+      `"Hi from @nuxtjs/i18n: hello"`
+    )
+  })
+})
