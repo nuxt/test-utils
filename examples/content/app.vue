85474405e3794e89d1911c9bdc77e07013ae21b9
Daniel Roe <daniel@roe.dev>
2023-12-05 21:15:22 +0000

    test: add example fixture with `@nuxt/content`

---
+++ examples/content/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Index page</div>
+  <div>title: {{ posts[0].title }}</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  value: 'set in index'
+})
+const posts = await queryContent('/').limit(1).find()
+</script>
