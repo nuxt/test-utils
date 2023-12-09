c58060092ca53fe755fd1582bea8d0b5311b5ccd
Daniel Roe <daniel@roe.dev>
2023-12-09 06:18:12 +0100

    fix: support typescript entry file

--- src/module/plugins/entry.ts
+++ src/module/plugins/entry.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'node:fs'
-import { dirname, join } from 'pathe'
+import { dirname, extname, join } from 'pathe'
 import { createUnplugin } from "unplugin"
 
 const PLUGIN_NAME = 'nuxt:vitest:nuxt-root-stub'
@@ -9,18 +9,22 @@ interface NuxtRootStubPluginOptions {
   rootStubPath: string
 }
 
+const STUB_ID = 'nuxt-vitest-app-entry'
+
 export const NuxtRootStubPlugin = createUnplugin((options: NuxtRootStubPluginOptions) => {
+  const STUB_ID_WITH_EXT = STUB_ID + extname(options.entry)
+
   return {
     name: PLUGIN_NAME,
     enforce: 'pre',
     vite: {
       async resolveId(id, importer) {
-        if (id.endsWith('nuxt-vitest-app-entry')) {
-          return importer?.endsWith('index.html') ? id : join(dirname(options.entry), 'nuxt-vitest-app-entry')
+        if (id.endsWith(STUB_ID) || id.endsWith(STUB_ID_WITH_EXT)) {
+          return importer?.endsWith('index.html') ? id : join(dirname(options.entry), STUB_ID_WITH_EXT)
         }
       },
       async load(id) {
-        if (id.endsWith('nuxt-vitest-app-entry')) {
+        if (id.endsWith(STUB_ID) || id.endsWith(STUB_ID_WITH_EXT)) {
           const entryContents = readFileSync(options.entry, 'utf-8')
           return entryContents.replace('#build/root-component.mjs', options.rootStubPath)
         }
