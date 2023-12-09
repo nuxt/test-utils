705e2aa505c16727fedda75d6ca41e41e6d0c1df
Daniel Roe <daniel@roe.dev>
2023-12-09 20:52:01 +0000

    test: add example fixture with `@nuxt/content` (#631)

---
+++ examples/content/tests/index.spec.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest'
+import { mockNuxtImport, mountSuspended } from '@nuxt/test-utils/runtime'
+
+import App from '~/app.vue'
+
+// Example usage: queryContent('/').limit(1).find()
+mockNuxtImport('queryContent', () => (_id: string) => ({
+  limit: (_limit: number) => ({
+    find: () => [{
+      title: 'My page'
+    }]
+  })
+}))
+
+describe('test utils', () => {
+  it('can mount components within nuxt suspense', async () => {
+    const component = await mountSuspended(App)
+    expect(component.html()).toMatchInlineSnapshot(`
+      "<div>Index page</div>
+      <div>title: My page</div>"
+    `)
+  })
+})
