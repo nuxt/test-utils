b7355dc436478ddc37f3fabb58c73e641a384f7b
Bobbie Goede <bobbiegoede@gmail.com>
2023-12-10 22:11:39 +0100

    test: confirm `setRuntimeConfig` works

--- examples/module/test/basic.test.ts
+++ examples/module/test/basic.test.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
 import { $fetch, setup } from '@nuxt/test-utils/e2e'
+import { setRuntimeConfig } from '@nuxt/test-utils/module-utils'
 
 describe('ssr', async () => {
   await setup({
@@ -10,6 +11,18 @@ describe('ssr', async () => {
   it('renders the index page', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
-    expect(html).toContain('<div>basic</div>')
+    expect(html).toContain('<div>basic <span>original value</span></div>')
+  })
+
+
+  it('changes runtime config and restarts', async () => {
+    const restoreConfig = await setRuntimeConfig({ public: { myValue: 'overwritten by test!' } })
+    
+    const html = await $fetch('/')
+    expect(html).toContain('<div>basic <span>overwritten by test!</span></div>')
+
+    await restoreConfig()
+    const htmlRestored = await $fetch('/')
+    expect(htmlRestored).toContain('<div>basic <span>original value</span></div>')
   })
 })
