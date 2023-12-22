dc205ba19badfc55e20f3c76dbdfb0628b6128d4
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2023-12-22 18:12:21 +1100

    Update i18n example test to actually exercise vuei18n

--- examples/i18n/app.vue
+++ examples/i18n/app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    Hi from @nuxtjs/i18n: {{ (('hello')) }}
+    Hi from @nuxtjs/i18n: {{ $t('hello') }}
+  </div>
+  <div>
+    Hi from @nuxtjs/i18n with useI18n: {{ str }}
   </div>
 </template>
+
+<script setup>
+const i18n = useI18n()
+
+const str = i18n.t("hello")
+</script>
