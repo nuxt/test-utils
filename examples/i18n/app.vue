8a2671e0624a59ccbc2fda93e7be38a33b5fec97
Daniel Roe <daniel@roe.dev>
2023-12-25 21:14:40 +0000

    test: update snapshot

--- examples/i18n/app.vue
+++ examples/i18n/app.vue
@@ -3,12 +3,12 @@
     Hi from @nuxtjs/i18n: {{ $t('hello') }}
   </div>
   <div>
-    Hi from @nuxtjs/i18n with useI18n: {{ str }}
+    Hi from @nuxtjs/i18n with useI18n: {{ setupTranslation }}
   </div>
 </template>
 
 <script setup>
 const i18n = useI18n()
 
-const str = i18n.t("hello")
+const setupTranslation = i18n.t('hello')
 </script>
