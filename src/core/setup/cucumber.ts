3938a1eccc0a4a524c85e62bb30e29aa00f00f9e
Daniel Roe <daniel@roe.dev>
2024-01-17 14:11:47 +0000

    chore: debug

--- src/core/setup/cucumber.ts
+++ src/core/setup/cucumber.ts
@@ -3,7 +3,13 @@ import type { TestHooks } from '../types'
 export default async function setupCucumber (hooks: TestHooks) {
   const { After, AfterAll, Before, BeforeAll } = await import('@cucumber/cucumber')
 
-  BeforeAll({ timeout: hooks.ctx.options.setupTimeout }, hooks.setup)
+  BeforeAll({ timeout: hooks.ctx.options.setupTimeout }, async () => {
+    try {
+      await hooks.setup()
+    } catch (error) {
+      console.error(error)
+    }
+  })
   Before({}, hooks.beforeEach)
 
   After(hooks.afterEach)
