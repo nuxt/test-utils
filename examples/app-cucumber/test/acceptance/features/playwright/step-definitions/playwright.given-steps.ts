d64959440a14ad98bd338f6b573367bdc0c336be
Daniel Roe <daniel@roe.dev>
2024-01-17 15:05:11 +0000

    test: add assertions to cucumber suite

--- examples/app-cucumber/test/acceptance/features/playwright/step-definitions/playwright.given-steps.ts
+++ examples/app-cucumber/test/acceptance/features/playwright/step-definitions/playwright.given-steps.ts
@@ -1,6 +1,15 @@
+import assert from 'node:assert'
 import { Given } from '@cucumber/cucumber'
-import { createPage } from '@nuxt/test-utils/e2e'
+import { $fetch, createPage } from '@nuxt/test-utils/e2e'
 
 Given(/^the user goes on the home page$/u, async function (): Promise<void> {
-  await createPage('/')
+  // Browser test
+  const page = await createPage('/')
+  const text = await page.getByRole('heading', { name: 'Welcome to Nuxt!' }).innerText()
+  assert.match(text, /Welcome to Nuxt!/)
+  await page.close()
+
+  // SSR test
+  const html: string = await $fetch('/')
+  assert.match(html, /<!DOCTYPE html>/)
 })
