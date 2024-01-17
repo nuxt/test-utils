22362211add4e25873b1207fbb0b72a67683cd8f
Daniel Roe <daniel@roe.dev>
2024-01-17 15:07:00 +0000

    fix: don't pass options to before

--- src/core/setup/cucumber.ts
+++ src/core/setup/cucumber.ts
@@ -4,7 +4,7 @@ export default async function setupCucumber (hooks: TestHooks) {
   const { After, AfterAll, Before, BeforeAll } = await import('@cucumber/cucumber')
 
   BeforeAll({ timeout: hooks.ctx.options.setupTimeout }, hooks.setup)
-  Before({}, hooks.beforeEach)
+  Before(hooks.beforeEach)
 
   After(hooks.afterEach)
   AfterAll(hooks.afterAll)
