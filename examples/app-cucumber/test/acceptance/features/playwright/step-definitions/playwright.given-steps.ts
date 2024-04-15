cd51e14ea7a3f7c749d1f1dd02a698884593b9fa
Daniel Roe <daniel@roe.dev>
2024-04-15 17:23:53 +0200

    chore: add non-null assertion

--- examples/app-cucumber/test/acceptance/features/playwright/step-definitions/playwright.given-steps.ts
+++ examples/app-cucumber/test/acceptance/features/playwright/step-definitions/playwright.given-steps.ts
@@ -6,7 +6,7 @@ Given(/^the user goes on the home page$/u, async function (): Promise<void> {
   // Browser test
   const page = await createPage('/')
   const text = await page.getByRole('heading', { name: 'Welcome to Nuxt!' }).textContent()
-  assert.match(text, /Welcome to Nuxt!/)
+  assert.match(text!, /Welcome to Nuxt!/)
   await page.close()
 
   // SSR test
