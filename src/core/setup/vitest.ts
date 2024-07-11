4074044471618ad18c2712b9241d6e37ed56a53e
Daniel Roe <daniel@roe.dev>
2024-07-11 07:52:24 +0100

    chore: allow `as any`

--- src/core/setup/vitest.ts
+++ src/core/setup/vitest.ts
@@ -3,6 +3,7 @@ import type { TestHooks } from '../types'
 export default async function setupVitest(hooks: TestHooks) {
   const vitest = await import('vitest')
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   hooks.ctx.mockFn = vitest.vi.fn as any
 
   vitest.beforeAll(hooks.setup, hooks.ctx.options.setupTimeout)
