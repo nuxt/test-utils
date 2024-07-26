acae14002d3d52b20d66652e5ebe5eaa7353c637
Daniel Roe <daniel@roe.dev>
2024-07-26 21:55:35 +0200

    chore: revert change from main

--- src/core/server.ts
+++ src/core/server.ts
@@ -32,7 +32,6 @@ export async function startServer(options: StartServerOptions = {}) {
         HOST: host,
         NODE_ENV: 'development',
         ...options.env,
-        ...ctx.options.env,
       },
     })
     await waitForPort(port, { retries: 32, host }).catch(() => {})
@@ -63,7 +62,6 @@ export async function startServer(options: StartServerOptions = {}) {
         HOST: host,
         NODE_ENV: 'test',
         ...options.env,
-        ...ctx.options.env,
       },
     })
     await waitForPort(port, { retries: 20, host })
