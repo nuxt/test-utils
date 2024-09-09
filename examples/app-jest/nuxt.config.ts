7723f969cc71792a556bf40209b9f5f723a2faaa
Daniel Roe <daniel@roe.dev>
2024-09-09 12:17:25 +0100

    chore: set `compatibilityDate` in fixtures

--- examples/app-jest/nuxt.config.ts
+++ examples/app-jest/nuxt.config.ts
@@ -2,5 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 })
--- examples/module/test/fixtures/basic/nuxt.config.ts
+++ examples/module/test/fixtures/basic/nuxt.config.ts
@@ -1,6 +1,7 @@
 import MyModule from '../../../src/module'
 
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
   runtimeConfig: {
     public: {
       myValue: 'original value',
