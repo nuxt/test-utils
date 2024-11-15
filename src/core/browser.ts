e87dc30f019d06fa8b6a7b9a011518637f85249d
Waly <ahmadouwalyndiaye@gmail.com>
2024-11-15 17:23:06 +0000

    feat: export NuxtPage interface to improve type safety in tests

--- src/core/browser.ts
+++ src/core/browser.ts
@@ -38,7 +38,7 @@ export interface GotoOptions extends Omit<_GotoOptions, 'waitUntil'> {
   waitUntil?: 'hydration' | 'route' | _GotoOptions['waitUntil']
 }
 
-interface NuxtPage extends Omit<Page, 'goto'> {
+export interface NuxtPage extends Omit<Page, 'goto'> {
   goto: (url: string, options?: GotoOptions) => Promise<Response | null>
 }
 
