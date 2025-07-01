b1ff702b784cb2dc722d7786f42f890f5ed7eb43
Tomina <tomina@vycital.eu>
2025-07-01 14:51:45 +0200

    fix(runtime-utils): handle computed defined using an object (#1342)

---
+++ examples/app-vitest-full/components/OptionsApiComputed.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li data-testid="simple-function">
+      {{ simpleFunction }}
+    </li>
+    <li data-testid="object-with-get">
+      {{ objectWithGet }}
+    </li>
+    <li data-testid="object-with-get-and-set">
+      {{ objectWithGetAndSet }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      localStateForObjectWithGetAndSet: 'object-with-get-and-set',
+    }
+  },
+  computed: {
+    simpleFunction() {
+      return 'simple-function'
+    },
+    objectWithGet: {
+      get() {
+        return 'object-with-get'
+      },
+    },
+    objectWithGetAndSet: {
+      get() {
+        return this.localStateForObjectWithGetAndSet
+      },
+      set(localStateForObjectWithGetAndSet) {
+        this.localStateForObjectWithGetAndSet = localStateForObjectWithGetAndSet
+      },
+    },
+  },
+}
+</script>
