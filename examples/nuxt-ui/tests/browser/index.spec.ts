08e7826cbe5946c7a4e05485e03d66bd1f49ad96
Jess <jess@jessicasachs.io>
2025-08-31 01:46:02 -0500

    test: add example tests for nuxt ui (#1396)

---
+++ examples/nuxt-ui/tests/browser/index.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest'
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import { render } from 'vitest-browser-vue'
+import App from '~/app.vue'
+
+describe('App', () => {
+  it('works with mountSuspended', async () => {
+    const component = await mountSuspended(App)
+    expect(component.html()).toMatchInlineSnapshot(`
+      "<div data-testid="page-content"> This is within a UApp component. </div>
+      <!--teleport start-->
+      <!--teleport end-->"
+    `)
+  })
+
+  it('works with vitest-browser-vue', () => {
+    const { getByText } = render(App)
+    expect(getByText('This is within a UApp component')).toBeInTheDocument()
+  })
+})
