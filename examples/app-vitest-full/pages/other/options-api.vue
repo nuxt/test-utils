d0fa82439060b4c9fecfb222ef7c31ca89447185
yamachi4416 <yamachi4416@gmail.com>
2025-11-05 22:25:12 +0900

    feat(runtime-utils): refactor mount + render helpers

--- examples/app-vitest-full/pages/other/options-api.vue
+++ examples/app-vitest-full/pages/other/options-api.vue
@@ -9,14 +9,29 @@
     <li data-testid="greeting-in-data2">
       {{ greetingInData2 }}
     </li>
+    <li data-testid="greeting-in-data3">
+      {{ greetingInData3 }}
+    </li>
+    <li data-testid="greeting-in-data4">
+      {{ greetingInData4 }}
+    </li>
     <li data-testid="greeting-in-computed">
       {{ greetingInComputed }}
     </li>
     <li data-testid="computed-data1">
       {{ computedData1 }}
     </li>
-    <li data-testid="computed-greeting-in-methods">
-      {{ computedGreetingInMethods }}
+    <li data-testid="computed-data2">
+      {{ computedData2 }}
+    </li>
+    <li data-testid="computed-with-methods">
+      {{ computedWithMethods }}
+    </li>
+    <li data-testid="computed-with-config">
+      {{ computedWithConfig }}
+    </li>
+    <li data-testid="computed-with-setup-ref">
+      {{ computedWithSetupRef }}
     </li>
     <li data-testid="greeting-in-methods">
       {{ greetingInMethods() }}
@@ -26,6 +41,18 @@
     </li>
     <li data-testid="return-computed-data1">
       {{ returnComputedData1() }}
+    </li>
+    <li data-testid="return-computed-data2">
+      {{ returnComputedData2() }}
+    </li>
+    <li data-testid="return-config-data">
+      {{ returnConfigData() }}
+    </li>
+    <li data-testid="return-ref-in-setup-data">
+      {{ returnRefInSetupData() }}
+    </li>
+    <li data-testid="return-props-data">
+      {{ returnPropsData() }}
     </li>
     <li>
       <button
@@ -49,9 +76,17 @@ export default defineNuxtComponent({
   components: {
     TestButton,
   },
+  props: {
+    prop1: {
+      type: String,
+      default: 'Hello, default',
+    },
+  },
   setup() {
+    const greetingRefInSetup = ref('Hello, setup')
     return {
       greetingInSetup: 'Hello, setup',
+      greetingRefInSetup,
     }
   },
   async asyncData() {
@@ -59,10 +94,12 @@ export default defineNuxtComponent({
       greetingInData2: 'Hello, overwritten by asyncData',
     }
   },
-  data() {
+  data({ $props }) {
     return {
       greetingInData1: 'Hello, data1',
       greetingInData2: 'Hello, data2',
+      greetingInData3: `Hello, ${this.$config.public.hello}`,
+      greetingInData4: $props.prop1,
     }
   },
   computed: {
@@ -72,8 +109,20 @@ export default defineNuxtComponent({
     computedData1() {
       return this.greetingInData1
     },
-    computedGreetingInMethods() {
+    computedData2() {
+      return this.greetingInData1.split(',')[0]
+    },
+    computedWithMethods() {
       return this.greetingInMethods()
+    },
+    computedWithConfig() {
+      return `Hello, ${this.$config.public.hello}`
+    },
+    computedWithSetupRef() {
+      return this.greetingRefInSetup.split(',')[0]
+    },
+    computedWithProps() {
+      return this.$props.prop1.split(',')[0]
     },
   },
   methods: {
@@ -85,6 +134,18 @@ export default defineNuxtComponent({
     },
     returnComputedData1() {
       return this.computedData1
+    },
+    returnComputedData2() {
+      return this.computedData1.split(',')[0]
+    },
+    returnConfigData() {
+      return `Hello, ${this.$config.public.hello}`
+    },
+    returnRefInSetupData() {
+      return this.greetingRefInSetup.split(',')[0]
+    },
+    returnPropsData() {
+      return this.$props.prop1.split(',')[0]
     },
     onClickButtonInPage() {
       if (this === undefined) {
