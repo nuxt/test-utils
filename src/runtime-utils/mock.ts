b6179fd4b3f96bdd2db2d07614b57df9421562ea
yamachi4416 <35223685+yamachi4416@users.noreply.github.com>
2025-12-04 22:02:18 +0900

    feat(runtime-utils): support mocked target arguments in mockNuxtImport (#1492)

--- src/runtime-utils/mock.ts
+++ src/runtime-utils/mock.ts
@@ -99,7 +99,7 @@ export function registerEndpoint(url: string, options: EventHandler | { handler:
 
 /**
  * `mockNuxtImport` allows you to mock Nuxt's auto import functionality.
- * @param _name - name of an import to mock.
+ * @param _target - name of an import to mock or mocked target.
  * @param _factory - factory function that returns mocked import.
  * @example
  * ```ts
@@ -110,11 +110,18 @@ export function registerEndpoint(url: string, options: EventHandler | { handler:
  *    return { value: 'mocked storage' }
  *  }
  * })
+ *
+ * // With mocked target
+ * mockNuxtImport(useStorage, () => {
+ *  return () => {
+ *    return { value: 'mocked storage' }
+ *  }
+ * })
  * ```
  * @see https://nuxt.com/docs/getting-started/testing#mocknuxtimport
  */
 export function mockNuxtImport<T = unknown>(
-  _name: string,
+  _target: string | T,
   _factory: () => T | Promise<T>,
 ): void {
   throw new Error(
