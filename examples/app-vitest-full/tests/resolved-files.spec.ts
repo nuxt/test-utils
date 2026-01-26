5b873dbcdebd466f93745a1ca1a514b96cab3089
Yoshihiro Yamaguchi <35223685+yamachi4416@users.noreply.github.com>
2026-01-26 23:25:38 +0900

    feat(runtime)!: change nuxt start timing to `beforeAll` hook (#1516)

--- examples/app-vitest-full/tests/resolved-files.spec.ts
+++ examples/app-vitest-full/tests/resolved-files.spec.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { test, expect } from 'vitest'
 import { createVitest } from 'vitest/node'
 
-test('it should include nuxt spec files', { timeout: 30000 }, async () => {
+test('it should include nuxt spec files', { timeout: 30000 }, async ({ onTestFinished }) => {
   const vitest = await createVitest('test', {
     config: fileURLToPath(new URL('../vitest.config.ts', import.meta.url)),
     dir: fileURLToPath(new URL('../', import.meta.url)),
@@ -10,21 +10,15 @@ test('it should include nuxt spec files', { timeout: 30000 }, async () => {
     run: false,
     watch: false,
   })
+
+  onTestFinished(async () => {
+    await vitest.close()
+  })
+
   const testFiles = await vitest.globTestSpecifications()
+  const nuxtSpecFiles = testFiles.filter(file => file.project.name === 'nuxt')
+  const regularSpecFiles = testFiles.filter(file => file.project.name === 'node')
 
-  await vitest.close()
-
-  const NUXT_PATH_RE = /[\\/]tests[\\/]nuxt[\\/]/
-  const nuxtSpecFiles = testFiles.filter(file => file.moduleId.endsWith('nuxt.spec.ts') || NUXT_PATH_RE.test(file.moduleId))
-  const regularSpecFiles = testFiles.filter(file => file.moduleId.endsWith('.spec.ts') && !file.moduleId.endsWith('nuxt.spec.ts') && !NUXT_PATH_RE.test(file.moduleId))
-
-  expect(nuxtSpecFiles.length).toEqual(25)
-  for (const file of nuxtSpecFiles) {
-    expect(file.project.name).toEqual('nuxt')
-  }
-
+  expect(nuxtSpecFiles.length).toEqual(26)
   expect(regularSpecFiles.length).toEqual(2)
-  for (const file of regularSpecFiles) {
-    expect(file.project.name).toEqual('node')
-  }
 })
