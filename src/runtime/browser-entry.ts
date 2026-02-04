ca06966102f69aeaf30df510f55d0d3e15ce0dde
Yoshihiro Yamaguchi <35223685+yamachi4416@users.noreply.github.com>
2026-02-04 21:48:52 +0900

    refactor(runtime): remove unnecessary querySelector (#1577)

--- src/runtime/browser-entry.ts
+++ src/runtime/browser-entry.ts
@@ -5,8 +5,5 @@ import type { NuxtWindow } from '../vitest-environment'
 import { setupNuxt } from './shared/nuxt'
 import { setupWindow } from './shared/environment'
 
-const el = document.querySelector(environmentOptions.nuxt.rootId || 'nuxt-test')
-if (!el) {
-  await setupWindow(window as unknown as NuxtWindow, environmentOptions)
-  await setupNuxt()
-}
+await setupWindow(window as unknown as NuxtWindow, environmentOptions)
+await setupNuxt()
