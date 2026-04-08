3ca540a33391e6b5dd007e70c2daa6894dc48d41
Yoshihiro Yamaguchi <35223685+yamachi4416@users.noreply.github.com>
2026-04-08 17:54:13 +0900

    test: change example/workspace to use glob based projects setup (#1585)

--- examples/app-vitest-workspace/vitest.config.ts
+++ examples/app-vitest-workspace/vitest.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vitest/config'
-import { defineVitestProject } from '@nuxt/test-utils/config'
 
 export default defineConfig({
   test: {
-    projects: [
-      await defineVitestProject({
-        test: {
-          name: 'nuxt',
-          include: ['**/*.nuxt.spec.ts'],
-          setupFiles: ['test/setup/setup-nuxt.ts'],
-        },
-      }),
-      {
-        test: {
-          name: 'unit',
-          include: ['**/*.unit.spec.ts'],
-        },
-      },
-    ],
+    projects: ['*/vitest*.config.ts'],
   },
 })
