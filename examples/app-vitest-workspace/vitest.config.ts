97404eb220f4b7e35f2cb8fdac6efdcf3e721f84
yamachi4416 <yamachi4416@gmail.com>
2026-05-06 21:39:05 +0900

    disable warning log

--- examples/app-vitest-workspace/vitest.config.ts
+++ examples/app-vitest-workspace/vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     projects: ['*/vitest*.config.ts'],
+    onConsoleLog(log) {
+      if (log.includes('<Suspense> is an experimental feature')) {
+        return false
+      }
+    },
   },
 })
