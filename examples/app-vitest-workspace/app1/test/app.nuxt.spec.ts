a59465b9e9dc90b365e83dfc01a1a62bfe2e9687
yamachi4416 <yamachi4416@gmail.com>
2026-05-08 22:37:48 +0900

    chore(e2e): add warning message for e2e in nuxt vitest setup

--- examples/app-vitest-workspace/app1/test/app.nuxt.spec.ts
+++ examples/app-vitest-workspace/app1/test/app.nuxt.spec.ts
@@ -4,4 +4,9 @@ describe('app', () => {
   it('useAppConfig', () => {
     expect(Object.keys(useAppConfig())).toEqual(['nuxt'])
   })
+
+  it('__NUXT_VITEST_RESOLVED__ is true', () => {
+    // @ts-expect-error injected global, not typed
+    expect(__NUXT_VITEST_RESOLVED__).toBe(true)
+  })
 })
