344b57420cbfe7966dc9bd1f89f6257c4a40adad
yamachi4416 <yamachi4416@gmail.com>
2026-06-25 00:20:04 +0900

    Remove vitest-browser-vue dependency

--- src/vitest-browser-nuxt/pure.ts
+++ src/vitest-browser-nuxt/pure.ts
@@ -1,20 +1,47 @@
-import { h, nextTick } from 'vue'
-import { render as wrapperFn } from 'vitest-browser-vue/pure'
+import type { Locator, LocatorSelectors, PrettyDOMOptions } from 'vitest/browser'
+import { page, server, utils } from 'vitest/browser'
+import type { VueWrapper } from '@vue/test-utils'
+import { mount as wrapperFn } from '@vue/test-utils'
 
-import type { WrapperSuspendedOptions, WrapperSuspendedResult } from '../runtime-utils/utils/suspended.ts'
+import type { WrapperSuspendedOptions } from '../runtime-utils/utils/suspended.ts'
 import { cleanupAll, wrapperSuspended } from '../runtime-utils/utils/suspended.ts'
 
-export { config, cleanup } from 'vitest-browser-vue/pure'
+export { config } from '@vue/test-utils'
 
 type WrapperFn<C> = typeof wrapperFn<C>
-type WrapperOptions<C> = WrapperSuspendedOptions<WrapperFn<C>>
-type WrapperResult<C> = WrapperSuspendedResult<WrapperFn<C>>
+type WrapperOptions<C> = WrapperSuspendedOptions<WrapperFn<C>> & {
+  container?: HTMLElement
+  baseElement?: HTMLElement
+}
+type ComponentProps<T> = T extends new (...args: never) => {
+  $props: infer P
+} ? NonNullable<P> : T extends (props: infer P, ...args: never) => unknown ? P : object
+
+export interface RenderResult<Props> extends LocatorSelectors {
+  container: HTMLElement
+  baseElement: HTMLElement
+  locator: Locator
+  debug(el?: HTMLElement | HTMLElement[] | Locator | Locator[], maxLength?: number, options?: PrettyDOMOptions): void
+  unmount(): Promise<void>
+  emitted<T = unknown>(): Record<string, T[]>
+  emitted<T = unknown[]>(eventName: string): undefined | T[]
+  rerender(props: Partial<Props>): Promise<void>
+}
+
+const { debug, getElementLocatorSelectors } = utils
+
+const mountedWrappers = new Set<Pick<VueWrapper, 'unmount'>>()
+
+let idx = 0
+function ensureTestIdAttribute(element: HTMLElement) {
+  const attributeId = server.config.browser.locators.testIdAttribute
+  if (!element.hasAttribute(attributeId)) {
+    element.setAttribute(attributeId, `__vitest_${idx++}__`)
+  }
+}
 
 /**
  * `render` allows you to mount any vue component within the vitest browser mode.
- *
- * This is a wrapper around the `render` function from vitest-browser-vue, and should be used together with
- * utilities from that package.
  *
  * ```ts
  * import { page } from 'vitest/browser'
@@ -41,45 +68,82 @@ type WrapperResult<C> = WrapperSuspendedResult<WrapperFn<C>>
 export async function render<T>(
   component: T,
   options: WrapperOptions<T> = {},
-): Promise<WrapperResult<T>> {
+): Promise<RenderResult<ComponentProps<T>>> {
   const suspendedHelperName = 'RenderBrowserHelper'
   const clonedComponentName = 'RenderBrowserSuspendedComponent'
 
   cleanupAll()
 
-  const umountedFns: (() => void)[] = []
-  const wrapperId = window.crypto.randomUUID()
+  if (options.attachTo) {
+    throw new Error('`attachTo` is not supported, use `container` instead')
+  }
 
-  const { wrapper: _wrapper, setProps } = await wrapperSuspended(component, options, {
+  let container!: HTMLElement
+  let baseElement!: HTMLElement
+
+  const { wrapper, setProps } = await wrapperSuspended(component, options, {
     wrapperFn,
-    wrappedRender: render => () => h({
-      inheritAttrs: false,
-      unmounted: () => umountedFns.forEach(fn => fn()),
-      render: () => h('div', { id: wrapperId }, render()),
-    }),
     overrideOptionsFn(options, vueApp) {
-      if (vueApp._container instanceof HTMLElement) {
-        options.container ??= vueApp._container
-      }
+      container ||= options.container || vueApp._container as HTMLElement
+      baseElement ||= options.baseElement || container || document.body
+      options.attachTo = container
     },
     suspendedHelperName,
     clonedComponentName,
     stubRouterLink: false,
   })
 
-  // The render result is wrapped in a PromiseLike object. Unwrap it to override.
-  const wrapper = await _wrapper
-  wrapper.rerender = async (props) => {
-    setProps(props)
-    await nextTick()
+  Object.assign(wrapper, { __setProps: setProps })
+
+  mountedWrappers.add(wrapper)
+
+  ensureTestIdAttribute(container)
+  ensureTestIdAttribute(baseElement)
+  unwrapNode(wrapper.element.parentElement)
+
+  const renderResult: RenderResult<ComponentProps<T>> = {
+    container,
+    baseElement,
+    locator: page.elementLocator(container),
+    debug: (el = container, maxLength, options) => debug(el, maxLength, options),
+    unmount: async () => {
+      wrapper.unmount()
+      mountedWrappers.delete(wrapper)
+      await mark(renderResult.locator, 'nuxt.unmount', renderResult.rerender)
+    },
+    emitted: ((name?: string) => wrapper.emitted(name!)) as never,
+    rerender: async (props) => {
+      await wrapper.setProps(props as never)
+      await mark(renderResult.locator, 'nuxt.rerender', renderResult.rerender)
+    },
+    ...getElementLocatorSelectors(container),
   }
 
-  // Unwrap render wrapper div element
-  const wrapperEl = document.getElementById(wrapperId)!
-  const unwrappedNodes = [...wrapperEl.childNodes]
-  wrapperEl.replaceWith(...unwrappedNodes)
+  return renderResult
+}
 
-  umountedFns.push(() => unwrappedNodes.forEach(c => c.remove()))
+export function cleanup(): void {
+  mountedWrappers.forEach((wrapper) => {
+    wrapper.unmount()
+    mountedWrappers.delete(wrapper)
+  })
+}
 
-  return wrapper
+async function mark(locator: Locator, name: string, fn: (...args: never) => unknown) {
+  if (!locator.mark) {
+    return
+  }
+
+  const error = new Error(name)
+  if ('captureStackTrace' in Error) {
+    Error.captureStackTrace(error, fn)
+  }
+
+  return locator.mark(name, error)
+}
+
+function unwrapNode(node: Element | null) {
+  if (node) {
+    node.replaceWith(...node.childNodes)
+  }
 }
