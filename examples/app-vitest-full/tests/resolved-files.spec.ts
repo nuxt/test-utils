36fab10f4e55b965cfa876b315720178d2c61a47
yamachi4416 <yamachi4416@gmail.com>
2026-08-02 11:44:53 +0900

    fix test timeout

--- examples/app-vitest-full/tests/resolved-files.spec.ts
+++ examples/app-vitest-full/tests/resolved-files.spec.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { test, expect } from 'vitest'
 import { createVitest } from 'vitest/node'
 
-test('it should include nuxt spec files', { timeout: 30000 }, async ({ onTestFinished }) => {
+test('it should include nuxt spec files', { timeout: process.env.CI ? 60_000 : 30_000 }, async ({ onTestFinished }) => {
   const vitest = await createVitest('test', {
     config: fileURLToPath(new URL('../vitest.config.ts', import.meta.url)),
     dir: fileURLToPath(new URL('../', import.meta.url)),
@@ -15,15 +15,20 @@ test('it should include nuxt spec files', { timeout: 30000 }, async ({ onTestFin
     await vitest.close()
   })
 
-  const testFiles = await vitest.collect()
+  const testFiles = await vitest.globTestSpecifications()
+  // const testFiles = await vitest.experimental_parseSpecifications(testSpecs)
 
-  const nuxtSpecFiles = testFiles.testModules.filter(file => file.project.name === 'nuxt')
-  const regularSpecFiles = testFiles.testModules.filter(file => file.project.name === 'node')
+  const nuxtSpecFiles = testFiles.filter(file => file.project.name === 'nuxt')
+  const regularSpecFiles = testFiles.filter(file => file.project.name === 'node')
 
   expect(nuxtSpecFiles.length).toEqual(30)
   expect(regularSpecFiles.length).toEqual(3)
 
   // Test files using `@vitest-environment node`
-  const nuxtNodeSpecFiles = nuxtSpecFiles.filter(file => file.viteEnvironment?.name === 'ssr')
-  expect(nuxtNodeSpecFiles.length).toEqual(2)
+  const nuxtNodeSpecs = await vitest.collectTests(nuxtSpecFiles.filter(file =>
+    file.moduleId.includes('/app-vitest-full/pages/')
+    || file.moduleId.includes('/app-vitest-full/composables/'),
+  ))
+  const nuxtNodeFiles = nuxtNodeSpecs.testModules.filter(module => module.viteEnvironment?.name === 'ssr')
+  expect(nuxtNodeFiles.length).toEqual(2)
 })
